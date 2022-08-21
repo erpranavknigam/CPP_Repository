9a395def9057ef6ecff78ba1c9b7ccd3ee642d3d
PRANAVKANT NIGAM <51795912+erpranavknigam@users.noreply.github.com>
2022-08-21 16:25:17 +0530

    circular subarray sum

--- Array/ArrayChallenges/max_circular_subarray_sum.cpp
+++ Array/ArrayChallenges/max_circular_subarray_sum.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int kadaneAlgorithm(int arr[], int n){
+    int currSum = 0;
+    int maxSum = INT_MIN;
+    for(int i = 0; i < n; i++){
+        currSum += arr[i];
+        if(currSum < 0){
+            currSum = 0;
+        }
+        maxSum = max(maxSum, currSum);
+    }
+   
+    return maxSum;
+}
 int main(){
     int size;
     
@@ -9,5 +22,20 @@ int main(){
     for (int i = 0; i < size; i++){
         cin >> arr[i];
     }
-    
+
+    int wrapsum;
+    int nonwrapsum;
+    int totalSum = 0;
+    nonwrapsum = kadaneAlgorithm(arr, size);
+    // cout << "nonwrapsum: " << nonwrapsum << endl;
+    for(int i = 0; i < size; i++){
+        totalSum += arr[i];
+        arr[i] = -arr[i];
+        // cout << arr[i] << " ";
+    }
+    // cout << endl;
+    // cout << "total sum: " << totalSum << endl;
+    wrapsum = totalSum + kadaneAlgorithm(arr, size);
+    // cout << "wrapsum: " << wrapsum << endl;
+    cout << max(wrapsum, nonwrapsum) << endl;
 }
